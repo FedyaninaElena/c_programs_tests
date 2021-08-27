33043b74de9c270481bab4976ea3020a094b8d7d
Елена Федянина <fedyanina.elenka@gmail.com>
2021-08-27 22:57:59 +0300

    Задача 002_003

---
+++ 002_003/c_002_003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+float f_002_003(float a);
+
+int main()
+{
+  float result_1 = f_002_003(56.46);
+  printf("%f\n", result_1);
+  float result_2 = f_002_003(-6.06);
+  printf("%f", result_2);
+
+  return 0;
+}
---
+++ 002_003/f_002_003.c
@@ -0,0 +1,8 @@
+#include <math.h>
+
+float f_002_003(float a)
+{
+  float S = M_PI * a * a;
+
+  return (S / 10000);
+}
