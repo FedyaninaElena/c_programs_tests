729760e81f3f2bfe77b102474af06a9e5926c1bd
Елена Федянина <fedyanina.elenka@gmail.com>
2021-08-27 16:00:13 +0300

    Задача 002_002

---
+++ 002_002/c_002_002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+float f_002_002(float a);
+
+float main()
+{
+  float result_1 = f_002_002(3.46);
+  printf("%f\n", result_1);
+  float result_2 = f_002_002(-2.05);
+  printf("%f", result_2);
+
+  return 0;
+}
